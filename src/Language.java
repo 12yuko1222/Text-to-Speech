6fa941207b4167bb64b48eca30ad11872f98a2fe
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-12-17 11:04:52 +0300

    add: Language class

---
+++ src/Language.java
@@ -0,0 +1,26 @@
+public class Language {
+
+    static final String ARABIC_VOICE = "Mehdi22k";
+    static final String ENGLISH_VOICE = "Heather22k";
+    private static String lang;
+
+    public static void setLang(String chose) throws InputException {
+        Messages.setLanguage(chose);
+
+        if (chose.equals("1")) {
+            lang = "ARABIC";
+        } else if (chose.equals("2")) {
+            lang = "ENGLISH";
+        }
+    }
+
+    public static String getLang() {
+        return lang;
+    }
+
+    public static String ChoseLang() {
+        if ("ARABIC".equals(lang)) return ARABIC_VOICE;
+        return ENGLISH_VOICE;
+    }
+
+}
